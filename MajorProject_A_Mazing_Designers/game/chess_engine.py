ff1decdd5e8c527cb5d64d3cb7b55825e8a3e317
Shyam-Parikh-2025 <shyamparikh3111@yahoo.com>
2026-02-15 22:06:27 -0500

    Added enhanced documentation
    Updated chess engine to improve functionality and added documentation.

--- MajorProject_A_Mazing_Designers/game/chess_engine.py
+++ MajorProject_A_Mazing_Designers/game/chess_engine.py
@@ -1,14 +1,28 @@
+# Documentation:
+# The pieces are stored as integers in the following binary method.
+# The 4th bit determines color: so, 0 for white, 1 for black
+# First 3 bits show type to (val 1-7) the piece types are title below.
+# (piece & 7) is used to find type and (piece & 8) to find color (Bitwise operations)
+# Example: 1110 (14 - shows black king = black(8) + king(6)=14)
+#          0110 (6 - shows white king = white(0) + king(6)=6)
+# Some of the complicated functions have docstring explaining their use.
+# A pseudo valid move is a move that a piece can do, but it is not checked that it would
+# cause check on the same color, so a black pawn moving can allow a check to the black king if
+# we just looked at pseudo valid moves, but valid moves are checked moves
+#  
 import pygame as pg
 import numpy as np
 import os
 from numba import njit
 from constants import ROWS, COLS, SQSIZE, WIDTH, HEIGHT, color
 
+# ============ PIECE TYPES =================
 w_p, w_k, w_b, w_r, w_q, w_K = 1, 2, 3, 4, 5, 6
 b_p, b_k, b_b, b_r, b_q, b_K = 9, 10, 11, 12, 13, 14
 empty = 0
 # black 1000 binary + piece value
 
+# ============ OFFSETS =================
 knight_offsets = ((-2, -1), (-2, 1), (2, -1), (2, 1), (1, 2), (1, -2), (-1, -2), (-1, 2))
 diag_offsets = ((-1, -1),(-1, 1),(1, 1),(1, -1))
 straight_offsets = ((-1,0),(1,0),(0,-1),(0,1))
@@ -16,11 +30,14 @@ king_offsets = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1),  (1, 0),  
 
 
 class Chess:
-    def __init__(self, points=None, super_mode=None):
+    def __init__(self, points=None, super_mode=0):
+
         self.points = points
         pawn_row = np.array([w_p] * COLS)
+        # To add exta queens if more dragons beaten
         if super_mode is not None:
-            idxs = np.random.choice(len(pawn_row), super_mode, replace=False)
+            cnt = min(super_mode, COLS)
+            idxs = np.random.choice(len(pawn_row), cnt, replace=False)
             pawn_row[idxs] = w_q
 
         self.board = np.array([
@@ -30,7 +47,7 @@ class Chess:
             [empty] * COLS,
             [empty] * COLS,
             [empty] * COLS,
-            [w_p] * COLS,
+            pawn_row,
             [w_r, w_k, w_b, w_q, w_K, w_b, w_k, w_r]
         ], dtype=np.int8)
         self.white_turn = True
@@ -39,6 +56,7 @@ class Chess:
         self.load_images()
     
     def make_move(self, move):
+        """ This makes the move and stores it in the move_log"""
         start, end = move
         start_row, start_col = start
         end_row, end_col = end
@@ -47,10 +65,12 @@ class Chess:
         captured_piece = self.board[end_row][end_col]
         self.board[end_row][end_col] = moved_piece
         self.board[start_row][start_col] = 0
+        # move_log syntax [(move, captured piece, whose turn)...]
         self.move_log.append((move, captured_piece, self.white_turn))
         self.white_turn = not self.white_turn
         
     def undo_move(self):
+        """ This undos the last move and removes it from the move_log"""
         if not self.move_log:
             return
         move, captured_piece, prev_turn = self.move_log.pop()
@@ -61,14 +81,18 @@ class Chess:
         self.white_turn = prev_turn
     
     def get_valid_moves(self):
+        """This function gets all pseudo valid moves and validates each one."""
         pseuodo_valid_moves = self.get_all_pos_moves()
         return self.validate_moves(pseuodo_valid_moves)
 
     def get_all_pos_moves(self):
+        """This is a method in the chess class that calls on a numba function with all
+        parameters added (numba can not accept self)"""
         board = self.board
         return get_pos_moves(self.white_turn, board)
 
     def load_images(self):
+        """This just loads the image with a library called Pathlib"""
         base_path = os.path.dirname(__file__)
         image_folder = os.path.join(base_path, "images")
 
@@ -83,6 +107,8 @@ class Chess:
                 self.images[key] = img
 
     def validate_moves(self, pseudo_moves):
+        """The validation function that calls on the helper function is_check to
+        validate pseudo moves"""
         valid_moves=[]
 
         for move in pseudo_moves:
@@ -96,6 +122,8 @@ class Chess:
         return valid_moves
 
     def is_in_check(self, is_white_king):
+        """Finds where the king is of the color and uses the is_square_attacked function
+        to find if the king is attacked"""
         king_val = w_K if is_white_king else b_K
         pos = np.where(self.board == king_val)
         if len(pos[0]) == 0: return False
@@ -104,6 +132,7 @@ class Chess:
         return is_square_attacked(self.board, king_row, king_col, attacker_is_white)
     
     def is_checkmate(self):
+        """is_checkmate function returns true if a color is checked and no other moves."""
         if self.is_in_check(self.white_turn):
             valid_moves = self.get_valid_moves()
             if len(valid_moves) == 0:
@@ -111,6 +140,7 @@ class Chess:
         return False
     
     def is_stalemate(self):
+        """is_stalemate function return true if a color is not checked and no other moves."""
         if not self.is_in_check(self.white_turn):
             if len(self.get_valid_moves()) == 0:
                 return True
@@ -118,6 +148,7 @@ class Chess:
 
 @njit
 def get_pos_moves(white_turn, board):
+    """This function uses numba and a helper function for each piece to find all pos moves"""
     moves = []
     for r in range(ROWS):
         for c in range(COLS):
@@ -159,6 +190,7 @@ def get_pos_moves(white_turn, board):
                     moves.append(m)
     return moves
 
+#============== HELPER FUNCTIONS FOR MOVE FINDING  =================
 @njit
 def pawn_moves(board, r, c, direction):
     moves = []
@@ -214,19 +246,25 @@ def king_moves(board, r, c, color):
 def in_range(num, st=0, en=8):
     return st<=num<en
 
+
 @njit
 def sliding_piece_move_finder(board, dirs, r, c, color, king=False):
+    """ This function is a helper function for the bishop, rook, king and queen and adds on each direction
+    until the board ends and/or a piece is found"""
     moves = []
     max_range = 2 if king else 8
-
+    # Iterate through all standard directions for this piece type
     for dr, dc in dirs:
         for i in range(1,max_range):
             target_r, target_c = r+dr*i, c+dc*i
-            if in_range(target_r) and in_range(target_c):
+            if in_range(target_r) and in_range(target_c):# 1. Check bounds
                 target = board[target_r][target_c]
-                if target == 0:
+                if target == 0: # 2. If Empty Square -> Add Move
                     moves.append(((r, c),(target_r, target_c)))
-                else:
+                else: # 3. If Occupied -> Check Color
+                    # Bitwise '& 8' extracts the color bit. 
+                    # If bytes match, it's a friendly piece (Stop).
+                    # If bytes differ, it's an enemy (Capture & Stop).
                     current_black = bool(board[r][c] & 8)
                     target_black = bool(target & 8)
                     if current_black != target_black: # if not same
@@ -237,7 +275,8 @@ def sliding_piece_move_finder(board, dirs, r, c, color, king=False):
 
 @njit
 def is_square_attacked(board, r, c, attacker_is_white):
-    
+    """Helper func of is_check and finds all moves that would have an end destination
+    at the square asked for."""
     attacker_is_black = not attacker_is_white
 
     if attacker_is_white:
